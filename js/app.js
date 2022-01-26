bced15307b88f83e6c5285a21f1158df33fc33e8
BarbaraKlimm <klimmbarbara@gmail.com>
2022-01-26 23:08:07 +0100

    Feat: used eslint to optimize js code

--- js/app.js
+++ js/app.js
@@ -19,55 +19,55 @@
  *
 */
 const sections = document.getElementsByTagName('section');
-const navbarList = document.getElementById('navbar__list');
-
-
+const navList = document.getElementById('navbar__list');
 // Build menu
 // build the nav
 // Scroll to anchor ID using scrollTO event
 
-  for (let section of sections){
-    const currentSection = section;
-    const newListElement = document.createElement('li');
-    const newLinkElement = document.createElement('a');
-    const currentSectionId = currentSection.id;
-    newLinkElement.setAttribute("class", "menu__link");
-    newLinkElement.textContent = currentSection.dataset.nav;
-    newLinkElement.classList.add(currentSectionId);
-    newLinkElement.addEventListener("click", function(e){
-      window.scrollBy({
-          top: section.getBoundingClientRect().top - navbarList.offsetHeight + 1,
+for (const section of sections) {
+  const currentSection = section;
+  const newListElement = document.createElement('li');
+  const newLinkElement = document.createElement('a');
+  const currentSectionId = currentSection.id;
+  newLinkElement.setAttribute('class', 'menu__link');
+  newLinkElement.textContent = currentSection.dataset.nav;
+  newLinkElement.classList.add(currentSectionId);
+  newLinkElement.addEventListener('click', function(e) {
+    window.scrollBy(
+        {
+          top: section.getBoundingClientRect().top - navList.offsetHeight + 1,
           left: 0,
-          behavior: 'smooth'
-        });
-      e.preventDefault();
-    });
-    newListElement.appendChild(newLinkElement);
-    navbarList.appendChild(newListElement);
-  }
+          behavior: 'smooth',
+        },
+    );
+    e.preventDefault();
+  });
+  newListElement.appendChild(newLinkElement);
+  navList.appendChild(newListElement);
+}
 
 
 // Set sections and link in navigation bar as active
 // Scroll to section on link click
-document.addEventListener('scroll', function(){
-  const navLinks = navbarList.getElementsByTagName('a');
+document.addEventListener('scroll', function() {
+  const navLinks = navList.getElementsByTagName('a');
   let current;
 
-    for (let section of sections){
-        if (section.getBoundingClientRect().top <= navbarList.offsetHeight) {
-          section.classList.add('your-active-class');
-          current = section.id;
-        } else {
-          section.classList.remove('your-active-class');
-        }
-    }
-
-    for (let navLink of navLinks){
-      if (navLink.classList.contains(current)){
-        navLink.classList.add('menu__link__active');
-      } else {
-        navLink.classList.remove('menu__link__active');
-      }
+  for (const section of sections) {
+    if (section.getBoundingClientRect().top <= navList.offsetHeight) {
+      section.classList.add('your-active-class');
+      current = section.id;
+    } else {
+      section.classList.remove('your-active-class');
     }
   }
-)
+
+  for (const navLink of navLinks) {
+    if (navLink.classList.contains(current)) {
+      navLink.classList.add('menu__link__active');
+    } else {
+      navLink.classList.remove('menu__link__active');
+    }
+  }
+},
+);
